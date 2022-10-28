4145dc9502d2ddd671bcbeeff0a356e9e14c382f
Ryuta339 <soccer.271828.339@gmail.com>
2022-10-29 00:38:37 +0900

    style: 本線を右側に配置した

--- src/components/templates/listItems.tsx
+++ src/components/templates/listItems.tsx
@@ -16,99 +16,99 @@ const routeMembers: RouteItemProps[] = [
   {
     depth: 0,
     index: 0,
-    label: "本線ー引上線",
+    label: "引上線ー本線",
     children: [
       {
         depth: 1,
         index: 1,
-        label: "1番線ー引上1番線",
+        label: "引上1番線ー1番線",
       },
       {
         depth: 1,
         index: 2,
-        label: "1番線ー引上2番線",
+        label: "引上2番線ー1番線",
       },
       {
         depth: 1,
         index: 3,
-        label: "1番線ー引上3番線",
+        label: "引上3番線ー1番線",
       },
       {
         depth: 1,
         index: 4,
-        label: "2番線ー引上1番線",
+        label: "引上1番線ー2番線",
       },
       {
         depth: 1,
         index: 5,
-        label: "2番線ー引上2番線",
+        label: "引上2番線ー2番線",
       },
       {
         depth: 1,
         index: 6,
-        label: "2番線ー引上3番線",
+        label: "引上3番線ー2番線",
       },
       {
         depth: 1,
         index: 7,
-        label: "3番線ー引上1番線",
+        label: "引上1番線ー3番線",
       },
       {
         depth: 1,
         index: 8,
-        label: "3番線ー引上2番線",
+        label: "引上2番線ー3番線",
       },
       {
         depth: 1,
         index: 9,
-        label: "3番線ー引上3番線",
+        label: "引上3番線ー3番線",
       },
       {
         depth: 1,
         index: 10,
-        label: "4番線ー引上1番線",
+        label: "引上1番線ー4番線",
       },
       {
         depth: 1,
         index: 11,
-        label: "4番線ー引上2番線",
+        label: "引上2番線ー4番線",
       },
       {
         depth: 1,
         index: 12,
-        label: "4番線ー引上3番線",
+        label: "引上3番線ー4番線",
       },
     ],
   },
   {
     depth: 0,
     index: 0,
-    label: "本線ー留置1〜5線",
+    label: "留置1〜5線ー1番線",
     children: [
       {
         depth: 1,
         index: 13,
-        label: "1番線ー留置1番線",
+        label: "留置1番線ー1番線",
       },
       {
         depth: 1,
         index: 14,
-        label: "1番線ー留置2番線",
+        label: "留置2番線ー1番線",
       },
       {
         depth: 1,
         index: 15,
-        label: "1番線ー留置3番線",
+        label: "留置3番線ー1番線",
       },
       {
         depth: 1,
         index: 16,
-        label: "1番線ー留置4番線",
+        label: "留置4番線ー1番線",
       },
       {
         depth: 1,
         index: 17,
-        label: "1番線ー留置5番線",
+        label: "留置5番線ー1番線",
       },
     ]
   },
